bba5758a45ad76d9509b5a8748a9057715fde661
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2023-04-25 00:01:53 +0100

    CRUD class method modified and updated/create and read method tested

--- api/controllers.py
+++ api/controllers.py
@@ -1,8 +1,9 @@
 from flask_restful import Resource, reqparse
 from api.models import Booking
+from api.utils import get_customer_data
 
 parser = reqparse.RequestParser()
-parser.add_argument('customer_id', type=str, required=True)
+parser.add_argument('customer_info', type=dict, required=True, help='Customer information must be a dictionary')
 parser.add_argument('booking_date', type=str, required=True)
 parser.add_argument('booking_time', type=str, required=True)
 parser.add_argument('booking_place', type=str, required=True)
@@ -19,13 +20,59 @@ class BookingList(Resource):
             results.append(booking)
         return results
 
-class BookingDetail(Resource):
-    def get(self, booking_id):
-        return Booking.get_by_id(booking_id)
 
 class BookingCreate(Resource):
     def post(self):
         args = parser.parse_args()
-        booking = Booking(args)
-        return {'booking_id': booking.booking_id}
+        customer_info = args.get('customer_info')
+        customer_id = customer_info.get('id')
+        customer_data = get_customer_data(customer_id)
+        if customer_data:
+            booking = Booking(customer=customer_data, # Pass customer_data as dictionary
+                              booking_date=args.get('booking_date'),
+                              booking_time=args.get('booking_time'),
+                              booking_place=args.get('booking_place'),
+                              service=args.get('service'),
+                              status=args.get('status'))
+            return {'booking_id': booking.booking_id}
+        else:
+            return {'error': 'Failed to fetch customer data'}, 400
 
+
+class BookingDetail(Resource):
+    def get(self, _id):
+        booking = Booking.get_by_id(_id)
+        if booking:
+            booking['_id'] = str(booking['_id'])
+            return booking
+        else:
+            return {'error': 'Booking not found'}, 404
+
+    def put(self, _id):
+        booking = Booking.get_by_id(_id)
+        if booking:
+            args = parser.parse_args()
+            customer_info = args.get('customer_info')
+            customer_id = customer_info.get('id')
+            customer_data = get_customer_data(customer_id)
+            if customer_data:
+                booking.customer = customer_data
+                booking.booking_date = args.get('booking_date')
+                booking.booking_time = args.get('booking_time')
+                booking.booking_place = args.get('booking_place')
+                booking.service = args.get('service')
+                booking.status = args.get('status')
+                booking.save()
+                return {'message': 'Booking updated successfully'}
+            else:
+                return {'error': 'Failed to fetch customer data'}, 400
+        else:
+            return {'error': 'Booking not found'}, 404
+
+    def delete(self, _id):
+        booking = Booking.get_by_id(_id)
+        if booking:
+            booking.delete()
+            return {'message': 'Booking deleted successfully'}
+        else:
+            return {'error': 'Booking not found'}, 404
