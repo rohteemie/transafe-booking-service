1cbbc38ef32c5d28a262cfd4e2db4beff1363735
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2023-04-24 03:01:18 +0100

    This is the main app file that with the api routes and flask app run method

---
+++ main.py
@@ -0,0 +1,12 @@
+from api import app
+from flask_restful import Api
+from api.controllers import BookingList, BookingDetail, BookingCreate
+
+api_route = Api(app)
+
+api_route.add_resource(BookingList, '/bookings')
+api_route.add_resource(BookingDetail, '/bookings/<booking_id>')
+api_route.add_resource(BookingCreate, '/bookings/new')
+
+if __name__ == '__main__':
+    app.run(debug=True)
