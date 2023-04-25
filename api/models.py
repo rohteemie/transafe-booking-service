2375f20efabc1acf49fbd84970a488d352db8e4f
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2023-04-25 01:27:44 +0100

    new methods added for crude operation to be used in api route define in api/controller.py module

--- api/models.py
+++ api/models.py
@@ -1,19 +1,40 @@
 from api import mongo
+from bson.objectid import ObjectId
 
 class Booking:
-    def __init__(self, data):
-        self.customer_id = data.get('customer_id')
-        self.booking_date = data.get('booking_date')
-        self.booking_time = data.get('booking_time')
-        self.booking_place = data.get('booking_place')
-        self.service = data.get('service')
-        self.status = data.get('status')
-        self.booking_id = str(mongo.db.bookings.insert_one(data).inserted_id)
+    def __init__(self, customer, booking_date, booking_time, booking_place, service, status):
+        self.customer = customer
+        self.booking_date = booking_date
+        self.booking_time = booking_time
+        self.booking_place = booking_place
+        self.service = service
+        self.status = status
+        data = {'customer': customer,
+                'booking_date': booking_date,
+                'booking_time': booking_time,
+                'booking_place': booking_place,
+                'service': service,
+                'status': status}
+        self._id = str(mongo.db.bookings.insert_one(data).inserted_id)
+
 
     @staticmethod
     def get_all():
         return list(mongo.db.bookings.find())
 
     @staticmethod
-    def get_by_id(booking_id):
-        return mongo.db.bookings.find_one({'booking_id': booking_id})
+    def get_by_id(_id):
+        booking_id = ObjectId(_id)
+        return mongo.db.bookings.find_one({'_id': booking_id})
+
+    def update(self):
+        data = {'customer': self.customer,
+                'booking_date': self.booking_date,
+                'booking_time': self.booking_time,
+                'booking_place': self.booking_place,
+                'service': self.service,
+                'status': self.status}
+        mongo.db.bookings.update_one({'_id': ObjectId(self.booking_id)}, {'$set': data})
+
+    def delete(self):
+        mongo.db.bookings.delete_one({'_id': ObjectId(self.booking_id)})
